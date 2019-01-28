8180d68d1efb83db69a9f7e4e04953339daa9b7b
yushi19791201 <yushiwh@126.com>
2019-01-28 14:12:05 +0800

    Feign的GZIP压缩   yushi

--- ch4-1/ch4-1-gzip/src/main/java/cn/springcloud/book/feign/controller/HelloFeignController.java
+++ ch4-1/ch4-1-gzip/src/main/java/cn/springcloud/book/feign/controller/HelloFeignController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+//http://localhost:8011/search/github?str=spring-cloud-dubbo
 @RestController
 public class HelloFeignController {
 
