a7da916b2a95a1e25a5a69913b0f7987288f8602
yushi19791201 <yushiwh@126.com>
2019-01-29 12:01:58 +0800

    基于配置文件和配置类的2种方法进行自定义配置，注意里面有排除扫描自定义类的方法

--- ch5-2/ch5-2-ribbon-loadbalancer/src/main/java/cn/springcloud/book/RibbonLoadbalancerApplication.java
+++ ch5-2/ch5-2-ribbon-loadbalancer/src/main/java/cn/springcloud/book/RibbonLoadbalancerApplication.java
@@ -17,17 +17,20 @@ import cn.springcloud.book.config.TestConfiguration;
 @SpringBootApplication
 @EnableDiscoveryClient
 @RibbonClient(name = "client-a", configuration = TestConfiguration.class)
+//可以使用@RibbonClients来配置多个源服务
 //@RibbonClients(value = {
 //		@RibbonClient(name = "client-a", configuration = TestConfiguration.class),
 //		@RibbonClient(name = "client-b", configuration = TestConfiguration.class)
 //})
+//让spring不去扫描被@AvoidScan标记的配置类，因为需要单个源服务进行生效。不排除就会报错
+//这里有个大坑，不这样写就要把配置类放在启动类的包和子包外面
 @ComponentScan(excludeFilters = {@ComponentScan.Filter(type = FilterType.ANNOTATION, value = {AvoidScan.class})})
 public class RibbonLoadbalancerApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(RibbonLoadbalancerApplication.class, args);
     }
-    
+
     @Bean
     @LoadBalanced
     public RestTemplate restTemplate() {
--- ch5-2/ch5-2-ribbon-loadbalancer/src/main/java/cn/springcloud/book/config/TestConfiguration.java
+++ ch5-2/ch5-2-ribbon-loadbalancer/src/main/java/cn/springcloud/book/config/TestConfiguration.java
@@ -8,10 +8,16 @@ import com.netflix.client.config.IClientConfig;
 import com.netflix.loadbalancer.IRule;
 import com.netflix.loadbalancer.RandomRule;
 
+/**
+ * 自定义的负载均衡类
+ * 全局策略设置
+ */
+
 @Configuration
 @AvoidScan
 public class TestConfiguration {
-	
+
+	//针对客户端的配置器
 	@Autowired
     IClientConfig config;
 
