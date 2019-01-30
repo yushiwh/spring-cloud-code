929b4ecf6de7e3efc63dbec3ed70e7f5bc22140e
yushi19791201 <yushiwh@126.com>
2019-01-30 18:35:17 +0800

    zuul的helloworld

--- ch7-2/ch7-2-client-a/src/main/java/cn/springcloud/book/controller/TestController.java
+++ ch7-2/ch7-2-client-a/src/main/java/cn/springcloud/book/controller/TestController.java
@@ -3,6 +3,12 @@ package cn.springcloud.book.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * 验证zuul的路由规则
+ * 1、http://localhost:7070/add?a=1&b=100           直接访问
+ * 2、http://localhost:5555/client/add?a=1&b=100    路由访问
+ */
+
 @RestController
 public class TestController {
 
