79794c3d4b9a8b507a7a83dcd30a8e55b0b541f2
yushi19791201 <yushiwh@126.com>
2019-01-31 09:12:02 +0800

    第一个自定义的Filter

--- ch8-1/ch8-1-zuul-server/src/main/java/cn/springcloud/book/filter/FirstPreFilter.java
+++ ch8-1/ch8-1-zuul-server/src/main/java/cn/springcloud/book/filter/FirstPreFilter.java
@@ -5,26 +5,55 @@ import static org.springframework.cloud.netflix.zuul.filters.support.FilterConst
 import com.netflix.zuul.ZuulFilter;
 import com.netflix.zuul.exception.ZuulException;
 
+/**
+ * 第一个自定义的filter，继承ZuulFilter，然后注入到springbean就可以使用
+ */
 public class FirstPreFilter extends ZuulFilter {
-	
-	@Override
-	public String filterType() {
-		return PRE_TYPE;
-	}
-	
-	@Override
-	public int filterOrder() {
-		return 0;
-	}
 
-	@Override
-	public boolean shouldFilter() {
-		return true;
-	}
+    /**
+     * 使用返回值设定Filter类型，可以设置pre,route,post,error
+     * <p>
+     * public static final String ERROR_TYPE = "error";
+     * public static final String POST_TYPE = "post";
+     * public static final String PRE_TYPE = "pre";
+     * public static final String ROUTE_TYPE = "route";
+     *
+     * @return
+     */
+    @Override
+    public String filterType() {
+        return PRE_TYPE;
+    }
 
-	@Override
-	public Object run() throws ZuulException {
-		System.out.println("这是第一个自定义Zuul Filter！");
-		return null;
-	}
+    /**
+     * 使用返回值设定Filter执行的顺序
+     *
+     * @return
+     */
+    @Override
+    public int filterOrder() {
+        return 0;
+    }
+
+    /**
+     * 使用返回值设定该Filter是否执行，可以作为开关来使用
+     *
+     * @return
+     */
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    /**
+     * Filter里面的核心执行逻辑，业务处理在此编写
+     *
+     * @return
+     * @throws ZuulException
+     */
+    @Override
+    public Object run() throws ZuulException {
+        System.out.println("这是第一个自定义Zuul Filter！");
+        return null;
+    }
 }
